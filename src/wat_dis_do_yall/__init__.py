628f88f0b14a7a7088f69e76b20d42d99a204f06
BenjaminWeiser <Benweiser04@gmail.com>
2025-05-16 01:12:15 -0400

    h

--- src/wat_dis_do_yall/__init__.py
+++ src/wat_dis_do_yall/__init__.py
@@ -1,29 +1,3 @@
-﻿# wat_dis_do_yall
+﻿from what_this_do_file import what_does_this_do
 
-A tiny example Python package that demonstrates how to publish/import
-from a GitHub repo. It provides one function, \what_does_this_do()\, which
-prints a message.
-
-## Usage
-
-\\\ash
-git clone https://github.com/yourusername/wat_dis_do_yall.git
-cd wat_dis_do_yall
-poetry install
-poetry run python example.py
-\\\
-
-You should see:
-
-\\\
-Hello from what_does_this_do()!
-\\\
-"@ | Out-File -Encoding UTF8 README.md
-
-# 6. Package __init__.py
-@"
-def what_does_this_do():
-    """
-    Print a simple message to stdout.
-    """
-    print("Hello from what_does_this_do()!")
+__all__ = ["what_does_this_do"]
